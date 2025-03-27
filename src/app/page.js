93d3741231cb6fae93cc3758c0b2827d17420440
Shravani Ladkat <shravaniladkat.03@gmail.com>
2025-03-27 20:12:52 +0530

    Navbar update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
+import HeroSection from "./components/hero-section";
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex justify-center items-center ">
-      This is home page
+    <>
+    <div className="w-full min-h-screen bg-gradient-to-b from-black to-red-900">
+    <Navbar />
+
+
     </div>
+    
+    </>
+      
   );
 }
